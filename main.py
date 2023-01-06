161559f942933be977309169a364c94f77302412
soha7893 <alskafys@gmail.com>
2023-01-06 20:08:19 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+class bus :
+    def __init__(self, bus_number,destination,seat ):
+        self.bus_number = bus_number
+        self.destination = destination
+        self.seat = seat
+
+
+
+bus1= bus(1,'from Gaza to yafa',25)
+bus2=bus(2,'from gaza to Hebron',25)
+bus3=bus(3,'from gaza to nablus',25)
+bus4=bus(4,'from gaza to nablus',25)
+bus5=bus(5,'from gaza to nablus',25)
